9e4c440f6b77f7af11e3e30a8ef7804c829bdf06
Diego Martinez <diegomrey1@gmail.com>
2024-03-09 10:37:45 +0100

    add timestamp

--- models/Article.js
+++ models/Article.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-  title: { type: String, require: true },
-  author: { type: String, require: true },
-  text: { type: String, require: true },
-});
+const articleSchema = new Schema(
+  {
+    title: { type: String, require: true },
+    author: { type: String, require: true },
+    text: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Article = mongoose.model("article", articleSchema);
 
