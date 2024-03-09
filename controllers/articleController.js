44b9a0ad74270da4816c12d09f61d8ab60ddac4c
badar <78179321+Badi21@users.noreply.github.com>
2024-03-09 11:03:38 +0100

    Adding coments to say how it works getAlllPosts

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,8 +3,12 @@ const Article = require("../models/Article");
 const articleController = {
   getAllPosts: async (req, res) => {
     try {
+      
+      // query vacia devolemos todos los posts
       const article = await Article.find();
       return res.json(article);
+      // comprobar la query con la expresion de mas de tres caracteres
+      // ir mostrando los articulos que coincidan en la base de datos
     } catch (error) {
       res.status(500).send("Unable to find posts");
     }
