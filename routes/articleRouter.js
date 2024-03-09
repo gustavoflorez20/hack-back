73cc7d5ee384b90c92e4ae1a40822a54904f6d98
badar <78179321+Badi21@users.noreply.github.com>
2024-03-09 12:24:33 +0100

    article route update

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const {articleController,addArticlecontroller } = require("../controllers/articleController");
+const articleController = require("../controllers/articleController");
 
 const router = express.Router();
 
 router.get("/", articleController.getAllPosts);
 
 router.get("/all", articleController.getAll);
-router.post ("/new", addArticlecontroller)
+router.post ("/new", articleController.addArticlecontroller)
 
 
 module.exports = router;
