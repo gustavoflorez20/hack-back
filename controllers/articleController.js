8233761f4186e433746812daba06faf438af48aa
Diego Martinez <diegomrey1@gmail.com>
2024-03-09 11:18:03 +0100

    update

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,9 +3,16 @@ const Article = require("../models/Article");
 const articleController = {
   getAllPosts: async (req, res) => {
     try {
-      
       // query vacia devolemos todos los posts
-      const article = await Article.find();
+      const articleText = req.query.text;
+
+      let filters = {};
+
+      if (Object.keys(articleText).length > 0) {
+        if (articleText.text) filters.text = articleText.text;
+      }
+
+      const article = await Article.find(filter);
       return res.json(article);
       // comprobar la query con la expresion de mas de tres caracteres
       // ir mostrando los articulos que coincidan en la base de datos
