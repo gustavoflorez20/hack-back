a557c97180206a2fea44c2948f8d69f6e18df2d4
badar <78179321+Badi21@users.noreply.github.com>
2024-03-09 12:06:24 +0100

    try getting all posts

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -7,14 +7,14 @@ const articleController = {
       const articleText = req.query.text;
 
       // Verificamos si el articleText tiene al menos tres caracteres alfabéticos
-      const regex = '/^[a-zA-Z]{3,}$/g';
+      const regex = new RegExp(`.*${articleText}.*`, 'i');
 
       if (!regex.test(articleText)) {
         return res.status(400).send("La consulta debe contener al menos tres caracteres alfabéticos.");
       }
 
       // Devolvemos los que coinciden con el article text
-      const articles = await Article.find({ title: { $regex: new RegExp(searchText, 'i') } });
+      const articles = await Article.find({ title: { $regex: regex} });
 
       if (articles.length === 0) {
         return res.status(404).send("No se encontraron artículos con el texto proporcionado.");
@@ -22,7 +22,8 @@ const articleController = {
 
       res.status(200).json(articles);
     } catch (error) {
-      res.status(500).send("Unable to find posts");
+      console.log(error)
+      res.status(500).json({estado_test: "Unable to find posts"});
     }
   },
 };
