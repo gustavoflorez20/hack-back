5fac67e377b711e2c716a3d7577173b0fd00a83b
Gustavo <148284090+gustavoflorez20@users.noreply.github.com>
2024-03-09 11:39:37 +0100

    2

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const {articleRouter , addArticle } = require("./routes/articleRouter");
-const { addArticle } = require("./controllers/articleController");
+const  articleRouter  = require("./routes/articleRouter");
+
 
 const port = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/", articleRouter);
-app.use ("/new",addArticle)
+app.use("/articles", articleRouter);
+
+app.post("/new", articleRouter);
 
 app.listen(port, () => {
   console.log("Rulandooooo server");
