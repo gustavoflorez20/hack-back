11408890a35be5972c8a7966e5636c8ceceb9f7e
WilliamMadrid <134057273+WilliamMadrid@users.noreply.github.com>
2024-03-09 11:02:46 +0100

    Add MongoDatabase

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const articleRouter = require("./routes/articleRouter");
+require('dotenv').config();
 
-const port = 3001;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", articleRouter);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Rulandooooo server");
 });
+
+//connect to MongoDb
+const mongoose = require('mongoose');
+const connectionStringToDb = `mongodb+srv://${process.env.DB_NAME}`
+
+async function main() {
+  return await mongoose.connect(connectionStringToDb)
+}
+ main()
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
